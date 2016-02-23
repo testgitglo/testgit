e68bbe27c86a3df10684863b093801e328a6d228
Hector Usma Montano <Hector Usma Montano>
2016-02-23 14:53:23 -0500

    ups

--- src/com/test/git/HelloWorld.java
+++ src/com/test/git/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test.git;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Worldddd"); 
-		System.out.println("effect of merge"); 
+		System.out.println("for merge"); 
 
 	}
 
