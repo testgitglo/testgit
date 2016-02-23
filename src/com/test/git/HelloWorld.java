233ec79a9b756d4191b0016ea15e58e7a48bac44
Hector Usma Montano <Hector Usma Montano>
2016-02-23 14:06:52 -0500

    commit for merge

--- src/com/test/git/HelloWorld.java
+++ src/com/test/git/HelloWorld.java
@@ -3,6 +3,7 @@ package com.test.git;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Worldddd"); 
+		System.out.println("master commit 22Feb"); 
 	}
 
 }
